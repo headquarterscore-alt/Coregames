5869d7713478cbc66959be551ef6c78c1af82517
headquarterscore-alt <headquarters.core@gmail.com>
2025-10-12 11:52:00 +0100

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,20 +5,9 @@ export default function Hero() {
     <>
       <Navbar />
 
-      <section className="pt-24 pb-8 px-6 relative overflow-hidden">
-        <div
-          className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-10"
-          style={{ backgroundImage: "url('/LOGO DuelCore (2000x2000) Henri.png')" }}
-        />
-
+      <section className="pt-20 pb-4 px-6 relative overflow-hidden">
         <div className="relative z-10 text-center max-w-5xl mx-auto">
-          <div className="flex justify-center">
-            <img
-              src="/LOGO DuelCore (2000x2000) Henri.png"
-              alt="DuelCore Logo"
-              className="h-40 w-40 object-contain animate-pulse"
-            />
-          </div>
+
         </div>
       </section>
     </>
